2c762e2fc71e58098bebe4a982126fcde58b9959
yeonj99 <yeonj99@naver.com>
2022-12-06 10:52:00 +0900

    java interface study

---
+++ src/interfaceex/Calc.java
@@ -0,0 +1,36 @@
+package interfaceex;
+
+public interface Calc {
+    double PI = 3.14;
+    int ERROR = -999999999;
+
+    //private 메서드
+    private void myMethod() {
+        System.out.println("private 메서드입니다.");
+    }
+    private static void myStaticMethod() {
+        System.out.println("private static 메서드입니다.");
+    }
+
+    //정적 메서드
+    static int total(int[] arr) {
+        int total = 0;
+
+        for (int i : arr) {
+            total += i;
+        }
+        myStaticMethod();
+        return total;
+    }
+
+    //디폴트 메서드
+    default void description() {
+        myMethod();
+        System.out.println("정수 계산기를 구현합니다.");
+    }
+    int add(int num1, int num2);
+    int substract(int num1, int num2);
+    int times(int num1, int num2);
+    int divide(int num1, int num2);
+    int square(int num);
+}
